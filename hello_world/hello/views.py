c28d588f60a86015aba2675d59f0877f1612f368
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2023-06-05 09:57:15 +0200

    Templates

--- hello_world/hello/views.py
+++ hello_world/hello/views.py
@@ -6,7 +6,17 @@ def home(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def home_who(request, who):
-    str_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    str_welcome = f"Hello <b>{who}</b>!<br> Now is {str_now}!"
+    # str_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # str_welcome = f"<h1>Hello <b>{who}</b>!</h1><br> Now is {str_now}!"
 
-    return HttpResponse( str_welcome )
+    # # sql = f"SELECT * FROM table WHERE name = {who}"
+
+    # return HttpResponse( str_welcome ) #+ "<br>" + sql )
+    return render(
+        request, 
+        'hello/index_who.html',     # nazwa szablonu
+        {                           # słownik zmiennych
+            'who': who,
+            'now': datetime.now()
+        }
+    )
